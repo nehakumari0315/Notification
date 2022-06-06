23d4ae76576bc00d62a45bac05848ca481f683fc
nehakumari0315 <erneha0315@gmail.com>
2022-06-06 22:16:35 +0530

    noticfication project

---
+++ app/Http/Controllers/NotificationController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\User;
+// use Notification;
+use App\Notifications\OffersNotification;
+use Illuminate\Support\Facades\Notification;
+
+class NotificationController extends Controller
+{
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
+  
+    public function index()
+    {
+        $user=User::first();
+        Notification::send($user, new OffersNotification);
+        return view('product');
+    }
+    
+    public function sendOfferNotification() {
+         $userSchema = User::first();
+  
+          $offerData = [
+            'name' => 'BOGO',
+            'body' => 'You received an offer.',
+            'thanks' => 'Thank you',
+            'offerText' => 'Check out the offer',
+            'offerUrl' => url('/'),
+            'offer_id' => 007
+        ];
+  
+          Notification::send($userSchema, new OffersNotification($offerData));
+   
+        dd('Task completed!');
+    }
+}
